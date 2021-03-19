59bd0b300a3045dd3c7fdeed98319b695d61bb91
Nikolaj J-K <kagenogmig@gmail.com>
2021-03-19 15:26:07 +0100

    Add checkWin to krollebot

--- krollebot/krollebot.ino
+++ krollebot/krollebot.ino
@@ -1,3 +1,7 @@
+const int boardSize = 3;
+
+int board [3][3];
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +10,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+bool checkWin() {
+  for (int i = 0; i < boardSize; i++) {
+    // tjek vandret
+    if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+      // vundet
+    }
+    // tjek lodret
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+      // vundet
+    }
+    // tjek diagonaler
+    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+        (board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
+      // vundet
+    }
+  }
 }
