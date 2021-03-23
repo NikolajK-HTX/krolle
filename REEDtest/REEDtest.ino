a76424f744c507a7e22be1428d9bbf93a1e663c4
Nikolaj J-K <kagenogmig@gmail.com>
2021-03-23 11:06:35 +0100

    REED test

---
+++ REEDtest/REEDtest.ino
@@ -0,0 +1,48 @@
+/*
+ * Lavet af Buster, Emil og Nikolaj Jehøj-Krogager
+ * 23/03/2021
+ * 
+ * Til test af REED switch grid. Du skal bruge følgende setup: 
+ *  - Clock til 4017 på pin 4
+ *  - REED ground til Arduino Uno pin 2
+ */
+
+const int reedInputPin = 2;
+const int counterClockPin = 4;
+
+bool reedBoard[9];
+bool pastReedBoard[9];
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(reedInputPin, INPUT);
+  pinMode(counterClockPin, OUTPUT);
+
+  for (int i = 0; i<9; i++) {
+    reedBoard[i] = false;
+    pastReedBoard[i] = false;
+  }
+}
+
+void loop() {
+  // læs alle REED switch og gem i array
+  for (int i = 0; i<9; i++) {
+    bool sensorVal = digitalRead(reedInputPin);
+    reedBoard[i] = sensorVal;
+    // Skift til næste REED switch
+    digitalWrite(counterClockPin, HIGH);
+    delay(5);
+    digitalWrite(counterClockPin, LOW);
+  }
+  // sammenlign de to arrays
+  for (int i = 0; i<9; i++) {
+    if (reedBoard[i] != pastReedBoard[i]) {
+      Serial.println("En brik er flyttet!");
+    }
+  }
+  // gem array
+  for (int i = 0; i<9; i++) {
+    pastReedBoard[i] = reedBoard[i];
+  }
+}
