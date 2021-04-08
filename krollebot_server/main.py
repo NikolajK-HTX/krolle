493b658b381bb6c723dd5d9b7231a206e2d38ba0
Nikolaj J-K <kagenogmig@gmail.com>
2021-04-08 19:33:37 +0200

    Create main.py

---
+++ krollebot_server/main.py
@@ -0,0 +1,33 @@
+import eventlet
+import socketio
+import serial
+import atexit # lukke for serial port når programmet lukker
+
+# websocket server
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio)
+
+# serial
+ser = serial.Serial('COM3')
+
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def krollebot_message(sid, data):
+    ser.write(str(data).encode())
+    print('Skriver til Arduino ', data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+def exit_handler():
+    ser.close()
+    print('Programmet lukkes')
+
+if __name__ == '__main__':
+    atexit.register(exit_handler)
+    eventlet.wsgi.server(eventlet.listen(('localhost', 5001)), app)
