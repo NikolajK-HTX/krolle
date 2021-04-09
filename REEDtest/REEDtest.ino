1aea85d252884371cbe21d399e262a1fdc1debb8
Nikolaj J-K <kagenogmig@gmail.com>
2021-04-09 11:45:05 +0200

    LED-Grid samt med REED-Grid

--- REEDtest/REEDtest.ino
+++ REEDtest/REEDtest.ino
@@ -5,11 +5,18 @@
    Til test af REED switch grid. Du skal bruge følgende setup:
     - Clock til 4017 på pin 4
     - REED ground til Arduino Uno pin 2
+
+   LED-Grid:
+    - Clock til LED-Grid 4017 på pin 7
+    - Reset til LED-Grid 4017 på pin 9
 */
 
 const int reedInputPin = 2;
 const int counterClockPin = 4;
 const int counterResetPin = 8;
+
+const int ledGridClockPin = 7;
+const int ledGridResetPin = 9;
 
 bool reedBoard[9];
 bool pastReedBoard[9];
@@ -28,6 +35,12 @@ void setup() {
     reedBoard[i] = false;
     pastReedBoard[i] = false;
   }
+
+  // LED-Grid
+  pinMode(ledGridClockPin, OUTPUT);
+  pinMode(ledGridResetPin, OUTPUT);
+  digitalWrite(ledGridClockPin, LOW);
+  digitalWrite(ledGridResetPin, LOW);
 }
 
 void loop() {
@@ -36,11 +49,17 @@ void loop() {
 
   // læs alle REED switch og gem i array
   for (int i = 0; i < 9; i++) {
+    delayMicroseconds(10);
     bool sensorVal = digitalRead(reedInputPin);
     reedBoard[i] = sensorVal;
     // Skift til næste REED switch
     clockCounter();
   }
+
+  for (int i = 0; i < 9; i++) {
+    Serial.print(reedBoard[i]);
+  }
+  Serial.println();
 
   // sammenlign de to arrays og sørg for at der er noget på
   // pladsen
@@ -54,16 +73,32 @@ void loop() {
   for (int i = 0; i < 9; i++) {
     pastReedBoard[i] = reedBoard[i];
   }
+
+  // reset LED-grid
+  digitalWrite(ledGridResetPin, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(ledGridResetPin, LOW);
+  // vis på LED-Grid
+  for (int i = 0; i < 9; i++) {
+    if(reedBoard[i] == true) {
+      delay(50);
+    }
+
+    // clock
+    digitalWrite(ledGridClockPin, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(ledGridClockPin, LOW);
+  }
 }
 
 void resetCounter() {
   digitalWrite(counterResetPin, HIGH);
-  delay(5);
+  delayMicroseconds(5);
   digitalWrite(counterResetPin, LOW);
 }
 
 void clockCounter() {
   digitalWrite(counterClockPin, HIGH);
-  delay(5);
+  delayMicroseconds(5);
   digitalWrite(counterClockPin, LOW);
 }
