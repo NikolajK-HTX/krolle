0ddc67b5ea8db0835a6568e76203c0d4e8ae8747
Nikolaj J-K <kagenogmig@gmail.com>
2021-04-24 18:56:27 +0200

    Tilføjet kommentarer

--- krollebot-node-server/index.js
+++ krollebot-node-server/index.js
@@ -1,12 +1,20 @@
+// Lavet af Buster, Emil og Nikolaj Jehøj-Krogager
+// i forbindelse med et eksamensprojekt i Elektronik & IT
+// 24/04/2021
+
+// bruges til at snakke med hjemmesiden
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
     cors: {
+        // gør så p5.js (samt alle andre hjemmesider) kan lave
+        // forespørgsler til denne server
         origin: '*',
     }
 });
 
 console.log("Programmet kører...");
 
+// bruges til at snakke med Arduinoen
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline')
 
@@ -14,6 +22,7 @@ const port = new SerialPort('COM3', {
     baudRate: 9600
 });
 
+// Gør så når der modtages data fra Arduinoen stopper den ved '\n'.
 const parser = port.pipe(new Readline())
 
 // modtager data fra Arduino Uno
@@ -27,9 +36,11 @@ parser.on('data', (data) => {
     }
 });
 
+// Denne funktion køres når en person tilslutter sig fra hjemmesiden
 io.on("connection", (socket) => {
     console.log("En person er forbundet :-)");
 
+    // modtager data fra hjemmesiden
     socket.on("krollebot_message", (message) => {
         port.write(message+'\n');
         console.log("Skriver til Arduino Uno: " + message);
